b5ba69614171dd052cc3370a55905b3dd5623d99
Noethys <info@noethys.com>
2016-07-17 23:51:15 +0200

    Ajout du fichier des versions

---
+++ connecthys/application/versions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------
+# Application :    Connecthys, le portail internet de Noethys
+# Site internet :  www.noethys.com
+# Auteur:          Ivan LUCAS
+# Copyright:       (c) 2010-16 Ivan LUCAS
+# Licence:         Licence GNU GPL
+#--------------------------------------------------------------
+
+# Exclusions possibles : ["application", "flask_adminlte", "lib"]
+
+VERSIONS = [
+    {"version" : "0.0.1",  "exclusions" : []},
+    {"version" : "0.0.2",  "exclusions" : ["flask_adminlte", "lib"]},
+    ]
+ 
