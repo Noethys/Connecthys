6f5d4e496c60dbe982b63fc425cb6057c419a383
bogucool <bogucool@gmail.com>
2016-08-17 14:30:48 +0200

    Ajout commentaires dans run.py

--- connecthys/run.py
+++ connecthys/run.py
@@ -13,5 +13,15 @@ import imports
 # Ajoute au path le chemin des librairies
 imports.AjouteCheminLibs()
 
+# Activation du SSL/TLS
+#import ssl
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#context.load_cert_chain('path/mon_certificat.crt', 'path/mon_certificat.key')
+
+
 from application import app
-app.run(port=5000, debug=True)
+# host='0.0.0.0': écoute sur toutes les interfaces réseaux de la machine (y compris loopback)
+# host='192.168.0.1': écoute sur l'interface ayant l'IP 192.168.0.1 uniquement
+# host='127.0.0.1': écoute uniquement sur l'interface loopback
+# host non défini: écoute uniquement sur l'interface loopback
+app.run(host='0.0.0.0', port=5000, debug=True)
