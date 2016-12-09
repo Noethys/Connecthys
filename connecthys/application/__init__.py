e0b4cf024aa36fa6ca48afee01de833ad4c36d09
Noethys <info@noethys.com>
2016-12-09 18:03:53 +0100

    Ajout noconfig

--- connecthys/application/__init__.py
+++ connecthys/application/__init__.py
@@ -27,10 +27,14 @@ __version__ = updater.GetVersionActuelle()
 app = Flask(__name__)
 
 # Configuration de flask
-from data.config import Config_application
-app.config.from_object(Config_application)
-app.config["VERSION_APPLICATION"] = __version__
-
+try :
+    from data.config import Config_application
+    app.config.from_object(Config_application)
+    app.config["VERSION_APPLICATION"] = __version__
+    config_ok = True
+except :
+    config_ok = False
+    
 # Flask compress
 #from flask_compress import Compress
 #compress = Compress()
@@ -51,51 +55,59 @@ handlerdebug.setFormatter(formatterdebug)
 app.logger.addHandler(handlerdebug)
 
 app.logger.setLevel(logging.DEBUG)
+
+if config_ok == True :
+
+    # Debugtoolbar
+    if Config_application.DEBUG == True :
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+        app.config['DEBUG_TB_PROFILER_ENABLED'] = True
+        from flask_debugtoolbar import DebugToolbarExtension
+        toolbar = DebugToolbarExtension(app)
+
+    # Connexion avec flask_login
+    from flask_login import LoginManager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    # Connexion avec SqlAlchemy
+    from flask_sqlalchemy import SQLAlchemy
+    db = SQLAlchemy(app)
+
+    # Connexion avec flask_migrate
+    from flask_script import Manager
+    from flask_migrate import Migrate, MigrateCommand
+    migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+
+    # Connexion avec AdminLTE
+    AdminLTE(app)
+
+    # Flask-WTF csrf protection
+    from flask_wtf.csrf import CsrfProtect 
+    csrf = CsrfProtect()  
+    csrf.init_app(app)
+
+    # Import des views et des models
+    import views, models, utils
+
+    # Recherche la version de la base de données
+    versionDB = models.GetVersionDB()
+
+    # Création de la base de données si nécessaire
+    if versionDB == None :
+        models.CreationDB()
+
+    # Vérifie que la DB est à jour
+    if versionDB != None :
+        if updater.GetVersionTuple(__version__) > updater.GetVersionTuple(versionDB) :
+            models.UpgradeDB()
+
+# Si configuration absente     
+if config_ok == False :
+    AdminLTE(app)
+    import noconfig
     
-# Debugtoolbar
-if Config_application.DEBUG == True :
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    app.config['DEBUG_TB_PROFILER_ENABLED'] = True
-    from flask_debugtoolbar import DebugToolbarExtension
-    toolbar = DebugToolbarExtension(app)
-
-# Connexion avec flask_login
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-# Connexion avec SqlAlchemy
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-
-# Connexion avec flask_migrate
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
-
-# Connexion avec AdminLTE
-AdminLTE(app)
-
-# Flask-WTF csrf protection
-from flask_wtf.csrf import CsrfProtect 
-csrf = CsrfProtect()  
-csrf.init_app(app)
-
-# Import des views et des models
-import views, models, utils
-
-# Recherche la version de la base de données
-versionDB = models.GetVersionDB()
-
-# Création de la base de données si nécessaire
-if versionDB == None :
-    models.CreationDB()
-
-# Vérifie que la DB est à jour
-if versionDB != None :
-    if updater.GetVersionTuple(__version__) > updater.GetVersionTuple(versionDB) :
-        models.UpgradeDB()
-
+    
---
+++ connecthys/application/noconfig.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------
+# Application :    Connecthys, le portail internet de Noethys
+# Site internet :  www.noethys.com
+# Auteur:          Ivan LUCAS
+# Copyright:       (c) 2010-16 Ivan LUCAS
+# Licence:         Licence GNU GPL
+#--------------------------------------------------------------
+
+from flask import Flask, render_template
+from application import app
+
+
+@app.route('/')
+def index():
+    return render_template('noconfig.html')
