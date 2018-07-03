6f0ac9cd7f1972c61b4fcd1570d81bc432903a71
Noethys <info@noethys.com>
2018-07-03 22:24:19 +0200

    Ajout de compare_type pour migrate

--- connecthys/application/__init__.py
+++ connecthys/application/__init__.py
@@ -78,7 +78,7 @@ if config_ok == True :
     # Connexion avec flask_migrate
     from flask_script import Manager
     from flask_migrate import Migrate, MigrateCommand
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, db, compare_type=True)
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
 
