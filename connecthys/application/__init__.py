797db0e3f2b6ac997562c40b18d9838ae4e8d351
Noethys <Noethys@users.noreply.github.com>
2020-06-19 20:05:21 +0200

    Petites modifications

--- connecthys/application/__init__.py
+++ connecthys/application/__init__.py
@@ -78,7 +78,10 @@ if config_ok == True :
     # Connexion avec flask_migrate
     from flask_script import Manager
     from flask_migrate import Migrate, MigrateCommand
-    migrate = Migrate(app, db, compare_type=True)
+    if db.engine.url.drivername == 'sqlite':
+        migrate = Migrate(app, db, compare_type=True, render_as_batch=True)
+    else:
+        migrate = Migrate(app, db, compare_type=True)
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
 
